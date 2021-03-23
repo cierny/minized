29b9afaf5818c18127545ec65ef5623b12cc96a5
cierny <o.cierny@gmail.com>
2021-03-22 20:53:48 -0400

    Boots linux without u-boot

---
+++ post_img.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cp ../output/fsbl/executable.elf "${BINARIES_DIR}/fsbl.elf"
+cp ../output/fsbl/minized_sbc_base.bit "${BINARIES_DIR}/fpga.bit"
+cp ../config/boot.bif "${BINARIES_DIR}"
+cd "${BINARIES_DIR}"
+bootgen -w on -image boot.bif -o i boot.bin
+rm fpga.bit boot.bif fsbl.elf zImage.minized
